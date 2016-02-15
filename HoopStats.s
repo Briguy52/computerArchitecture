a9222af24e2565e299a1ef5b1b8d54d7c35cd311
Briguy52 <Briguy52@gmail.com>
2016-02-15 14:47:53 -0500

    JPG calculated successfully, TODO: figure out how to store in linked list

--- HoopStats.s
+++ HoopStats.s
@@ -1,13 +1,14 @@
 .data
 nln: .asciiz "\n"
-askName: .asciiz "What is the player name? \n"
+askName: .asciiz "What is the player name?"
 askJersey: .asciiz "What is the jersey number?"
 askPoints: .asciiz "How many points per game?"
-test: .asciiz "\ntest\n"
-flag: .asciiz "flag\n"
+test: .asciiz "test\n"
+flag: .asciiz "Ending cycle\n"
 done: .asciiz "DONE"
 full: .space 72
 name: .space 64
+nameAndJPG: .space 68
 jpg: .space 4
 next: .space 4
 # Syscall quick reference
@@ -37,18 +38,20 @@ next: .space 4
 	.globl main
 
 main:
-	# li $v0, 9 # malloc syscall
-	# # la $a0, full # allot full buffer
-	# li $a0, 72
-	# syscall
-	# # move $s0, $v0 # store address of head node
-	# add $s0, $v0, $0
-	#
-	# li $v0, 9 # malloc syscall
-	# # la $a0, full # allot full buffer
-	# li $a0, 72
-	# syscall
-	# move $s1, $v0 # store address of current node
+	li $s7, 0 # head node empty? 0 = empty, 1 = not empty
+	
+	li $v0, 9 # malloc syscall
+	# la $a0, full # allot full buffer
+	li $a0, 72
+	syscall
+	# move $s0, $v0 # store address of head node
+	add $s0, $v0, $0
+
+	li $v0, 9 # malloc syscall
+	# la $a0, full # allot full buffer
+	li $a0, 72
+	syscall
+	move $s1, $v0 # store address of current node
 	
 askForString:	
 	li $v0, 4 # print string
@@ -59,7 +62,7 @@ askForString:
 	# la $a0, full # prep node
 	li $a0, 72
 	syscall
-	move $s2, $v0 # store address of allocated space
+	move $s2, $v0 # TODO: store address of allocated space 
 	
 	li $v0, 8 # read string
 	move $a0, $s2 # address for string to be stored
@@ -70,54 +73,40 @@ askForString:
 	
 	j checkDone
 	
-	li $v0, 8 # read string
-	move $a0, $v0
-	la $a0, name
-	move $t0, $a0
-	syscall
-	
-	li $v0, 4 # print input name
-	move $a0, $t0
-	syscall
-	
-	# check if name is equal to DONE 
-	move $a0, $t0 
-	jal checkDone
-	
-	bnez $v0, continue  # check result
-	
-	# li $v0, 4
-	# la $a0, test
-	# syscall
-	
-	j print
-	
 	continue:
 		la $a0, askJersey
 		li $v0, 4
 		syscall
 		
 		li $v0, 5  # 1) load in word/argument    
-		move $t1, $a0 # store        
 		syscall  
+		move $t1, $v0
 		
 		la $a0, askPoints
 		li $v0, 4
 		syscall
 		
 		li $v0, 5  # 1) load in word/argument    
-		move $t2, $a0 # store        
 		syscall  
+		move $t2, $v0
 		
-		# TODO: store t0, t1, and t2 in a struct
-		# j print
+		sub $t3, $t1, $t2 # JPG = jersey - points
+		
+		move $a0, $t3
+		li $v0, 1
+		syscall 
+		
+		sw $t3, name($s2) # move name bytes down and store your JPG
+
+		beqz $s7, newLL
+		
 		j askForString
 	
 checkDone:  
+	# TODO: great!
 		lb $t1($a0) # first char of input
 		la $t2, done # load "D"
 		lb $t3($t2) # first char of DONE (D)
-
 		
 		bne $t1, $t3, continue # ask for jersey/points
 
@@ -147,7 +136,18 @@ checkDone:
 		syscall
 
 		j print
-				 
+
+newLL: 
+	li $s7, 1 # set head to NON-EMPTY
+	addi $s0, $s2, 0 # add new node ($s2) to head ($s0) 
+	addi $s1, $s0, 0 # add these next node in list ($s1) 
+	j askForString # begin next cycle
+	
+existingLL:
+	sw $s2, nameAndJPG($s1) # move along next node (past already stored name + JPG)
+	move $s1, $s2 # shift pointers along
+	j askForString # begin next cycle
+	
 print:
 	li $v0, 10 # service for leaving
 	syscall
