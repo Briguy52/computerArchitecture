bac8c3e8468c3484b34378f1fa3b00b9103433e8
Briguy52 <Briguy52@gmail.com>
2016-02-15 14:07:18 -0500

    Success: can now compare strings

--- HoopStats.s
+++ HoopStats.s
@@ -3,13 +3,9 @@ nln: .asciiz "\n"
 askName: .asciiz "What is the player name? \n"
 askJersey: .asciiz "What is the jersey number?"
 askPoints: .asciiz "How many points per game?"
-test: .asciiz "test\n"
+test: .asciiz "\ntest\n"
 flag: .asciiz "flag\n"
-done: .asciiz "DONE\n"
-D: .asciiz "D"
-O: .asciiz "O"
-N: .asciiz "N"
-E: .asciiz "E"
+done: .asciiz "DONE"
 full: .space 72
 name: .space 64
 jpg: .space 4
@@ -41,18 +37,18 @@ next: .space 4
 	.globl main
 
 main:
-	li $v0, 9 # malloc syscall
-	# la $a0, full # allot full buffer
-	li $a0, 72
-	syscall
-	# move $s0, $v0 # store address of head node
-	add $s0, $v0, $0
-
-	li $v0, 9 # malloc syscall
-	# la $a0, full # allot full buffer
-	li $a0, 72
-	syscall
-	move $s1, $v0 # store address of current node
+	# li $v0, 9 # malloc syscall
+	# # la $a0, full # allot full buffer
+	# li $a0, 72
+	# syscall
+	# # move $s0, $v0 # store address of head node
+	# add $s0, $v0, $0
+	#
+	# li $v0, 9 # malloc syscall
+	# # la $a0, full # allot full buffer
+	# li $a0, 72
+	# syscall
+	# move $s1, $v0 # store address of current node
 	
 askForString:	
 	li $v0, 4 # print string
@@ -64,11 +60,6 @@ askForString:
 	li $a0, 72
 	syscall
 	move $s2, $v0 # store address of allocated space
-	
-	# li $a0, 72 # DONE - allot String buffer
-	# li $v0, 9 # TODO: read w/ malloc
-	# syscall
-	# add $s1, $v0, $zero #Save s1 as the head, set it as empty
 	
 	li $v0, 8 # read string
 	move $a0, $s2 # address for string to be stored
@@ -95,9 +86,9 @@ askForString:
 	
 	bnez $v0, continue  # check result
 	
-	li $v0, 4
-	la $a0, test
-	syscall
+	# li $v0, 4
+	# la $a0, test
+	# syscall
 	
 	j print
 	
@@ -123,67 +114,39 @@ askForString:
 		j askForString
 	
 checkDone:  
-		# move $t0, $a0 # store input string
 		lb $t1($a0) # first char of input
-		la $t2, D # load "D"
+		la $t2, done # load "D"
 		lb $t3($t2) # first char of DONE (D)
+
 		
-		li $v0, 4 # print string
-		la $a0, test
-		syscall
-		
-		bne $t1, $t2, continue # ask for jersey/points
+		bne $t1, $t3, continue # ask for jersey/points
 
 		addi $a0, $a0, 1 # shift over to next letter
 		lb $t1($a0) # first char of input
-		la $t2, O # load "O"
+		addi $t2, $t2, 1
 		lb $t3($t2) # second char of DONE (O)
 		
-		bne $t1, $t2, continue # ask for jersey/points
+		bne $t1, $t3, continue # ask for jersey/points
 		
 		addi $a0, $a0, 1 # shift over to next letter
 		lb $t1($a0) # first char of input
-		la $t2, O # load "N"
+		addi $t2, $t2, 1
 		lb $t3($t2) # third char of DONE (N)
 		
-		bne $t1, $t2, continue # ask for jersey/points
+		bne $t1, $t3, continue # ask for jersey/points
 		
 		addi $a0, $a0, 1 # shift over to next letter
 		lb $t1($a0) # first char of input
-		la $t2, O # load "E"
+		addi $t2, $t2, 1
 		lb $t3($t2) # fourth char of DONE (E)
 		
-		bne $t1, $t2, continue # ask for jersey/points
+		bne $t1, $t3, continue # ask for jersey/points
 		
 		li $v0, 4 # print string
 		la $a0, flag
 		syscall
-		
-		addi $a0, $a0, 1 # shift over to next letter
-		lb $t1($a0) # first char of input
-		la $t2, nln # load "E"
-		lb $t3($t2) # fourth char of DONE (E)
-		
-		bne $t1, $t2, continue # ask for jersey/points
-		
+
 		j print
-		
-		
-		# move $t0, $a0
-		# la $t1, done
-		# li $t4, 0 # counter variable
-		# li $t5, 4
-		# j loop
-		#
-		# loop:
-		# 	beq $t4, $t5, continue
-		# 	lb $t2($t0)
-		# 	lb $t3($t1)
-		# 	bne $t3, $t2, continue # ask for jersey/points
-		# 	addi $t0, $t0, 1
-		# 	addi $t1, $t1, 1
-		# 	addi $t4, $t4, 1
-		# 	j loop
 				 
 print:
 	li $v0, 10 # service for leaving
